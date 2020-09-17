1eb0c5d1922068ca864294e1d507096c1785c0d9
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-09-18 01:48:41 +0200

    Install cors dependencies and integrate it in the app

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import path from 'path';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import cors from 'cors';
 
 import {buildControllers} from './api/controllers';
 import {addInjectableService} from './core';
@@ -18,6 +19,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // loading the controllers
