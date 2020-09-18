5b599aa297562bc253d3e8862eea3d6c5657040b
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-09-18 22:56:30 +0200

    Update random color function

--- game/strategy.js
+++ game/strategy.js
@@ -9,10 +9,10 @@ export class Strategy {
     }
 
     getRandomColor() {
-        const letters = '0123456789ABCDEF';
+        const letters = '123456789ABCDE';
         let color = '#';
         for (let i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
+            color += letters[Math.floor(Math.random() * letters.length)];
         }
         return color;
     }
