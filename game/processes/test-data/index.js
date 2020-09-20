38dd00831431edc306a08602ff3c7a4586cde551
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-09-20 22:20:54 +0200

    Create new test cases for validate the strategy

--- game/processes/test-data/index.js
+++ game/processes/test-data/index.js
@@ -1,5 +1,7 @@
 import input1 from './initial-validation';
+import input2 from './manual-validation';
 
 export default [
-    input1
+    input1,
+    input2,
 ];
---
+++ game/processes/test-data/manual-validation.js
@@ -0,0 +1,87 @@
+export default [
+    {
+        vision:
+            [
+                [1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1],
+                [1, 1, 0, 1, 1],
+                [1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1]],
+        players: [],
+        position: {x: 2, y: 2},
+        velocity: 2,
+        health: 100,
+        attack: 10,
+        name: 'qfxuh'
+    },
+    {
+        vision:
+            [
+                [1, 1, 1, 1, 1],
+                [1, 0, 0, 0, 1],
+                [1, 0, 0, 0, 1],
+                [1, 0, 0, 0, 1],
+                [1, 1, 1, 1, 1]],
+        players: [],
+        position: {x: 2, y: 2},
+        velocity: 2,
+        health: 100,
+        attack: 10,
+        name: 'qfxuh'
+    },
+    {
+        vision:
+            [
+                [1, 1, 0, 1, 1],
+                [1, 1, 0, 1, 1],
+                [1, 0, 0, 0, 1],
+                [1, 1, 0, 1, 1],
+                [1, 1, 0, 1, 1]],
+        players: [],
+        position: {x: 2, y: 2},
+        velocity: 2,
+        health: 100,
+        attack: 10,
+        name: 'qfxuh'
+    },
+    {
+        vision:
+            [
+                [1, 1, 1, 1],
+                [1, 0, 0, 1],
+                [1, 0, 0, 1],
+                [1, 1, 1, 1]],
+        players: [],
+        position: {x: 2, y: 2},
+        velocity: 2,
+        health: 100,
+        attack: 10,
+        name: 'qfxuh'
+    },
+    {
+        vision:
+            [
+                [1, 1, 0],
+                [0, 0, 0],
+                [0, 0, 0]],
+        players: [],
+        position: {x: 1, y: 1},
+        velocity: 2,
+        health: 100,
+        attack: 10,
+        name: 'qfxuh'
+    },
+    {
+        vision:
+            [
+                [1, 1, 0],
+                [0, 0, 0],
+                [0, 0, 0]],
+        players: [],
+        position: {x: 2, y: 2},
+        velocity: 2,
+        health: 100,
+        attack: 10,
+        name: 'qfxuh'
+    }
+];
