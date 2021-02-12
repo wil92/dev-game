be2f21fad55c415bc75f62bd25479b16395f0862
Guillermo Gonzalez <ggjnez92@gmail.com>
2021-02-12 02:41:05 +0100

    Update points model

--- api/models/points.js
+++ api/models/points.js
@@ -1,11 +1,21 @@
 import {Attribute, Model, ToOne} from '../../core';
 import User from "./user";
+import Position from "./position";
 
 @Model()
 export default class Points {
     @Attribute({type: Number})
     value;
 
+    @Attribute({type: Number})
+    difference;
+
+    @Attribute({type: Number})
+    position;
+
     @ToOne({model: User})
     user;
+
+    @ToOne({model: Position})
+    standing;
 }
